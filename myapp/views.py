8a747f307cc95985ad1cca3f2ec934807625a239
Chidera C. Onwurah <69947729+dera001@users.noreply.github.com>
2021-08-15 05:54:14 +0100

    Add files via upload

---
+++ myapp/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import redirect, render
+from .models import Document
+from .forms import DocumentForm
+
+
+def my_view(request):
+    message = 'Upload your assignment !'
+    # Handle file upload
+    if request.method == 'POST':
+        form = DocumentForm(request.POST, request.FILES)
+        if form.is_valid():
+            newdoc = Document(docfile=request.FILES['docfile'])
+            newdoc.save()
+
+            # Redirect to the document list after POST
+            return redirect('my-view')
+        else:
+            message = 'The form is not valid. Fix the following error:'
+    else:
+        form = DocumentForm()  # An empty, unbound form
+
+    # Load documents for the list page
+    documents = Document.objects.all()
+
+    # Render list page with the documents and the form
+    context = {'documents': documents, 'form': form, 'message': message}
+    return render(request, 'list.html', context)
